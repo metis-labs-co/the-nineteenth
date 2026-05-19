5b9f42ddc9031a42241dfcfa1984514a2c5e965c
Sam Kay <sam@metislabs.co>
2026-05-19 14:27:51 +1000

    feat(competition-detail): export WhatsAppGroupSection

--- src/components/competitions/detail/sections/index.ts
+++ src/components/competitions/detail/sections/index.ts
@@ -9,6 +9,7 @@ export { MiniLeaderboardSection } from './MiniLeaderboardSection';
 export { InProgressRoundSection } from './InProgressRoundSection';
 export { SettingsSection } from './SettingsSection';
 export { PrizePoolSection } from './PrizePoolSection';
+export { WhatsAppGroupSection } from './WhatsAppGroupSection';
 
 // Re-export types
 export * from './types';
